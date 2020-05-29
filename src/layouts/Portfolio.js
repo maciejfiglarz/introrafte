c1e5a8498ad92480fca187383aada17f3e126ef9
Maciej Figlarz <maciejfiglarz333@gmail.com>
2020-05-29 15:48:41 +0200

    implement lightbox

--- src/layouts/Portfolio.js
+++ src/layouts/Portfolio.js
@@ -1,5 +1,6 @@
 import React from "react";
 import photo from "./../img/print.jpg";
+import Lightbox from 'react-lightbox-component';
 
 const getStyle = (linkPhoto) => {
   return {
@@ -11,25 +12,47 @@ const getStyle = (linkPhoto) => {
 
 const Portfolio = () => {
   return (
-    <div className="portfolio">
-      <div className="portfolio__container container">
-        <div className="portfolio__title">OSTATNIE REALIZACJE</div>
-        <div className="portfolio__item-wrap">
-          {/* {images.map((photo) => ( */}
-          <div style={getStyle(photo)} className="portfolio__item"></div>
-          <div style={getStyle(photo)} className="portfolio__item"></div>
-          <div style={getStyle(photo)} className="portfolio__item"></div>
-          <div style={getStyle(photo)} className="portfolio__item"></div>
+    <>
+      <div className="portfolio">
+        <div className="portfolio__container container">
+          <div className="portfolio__title">OSTATNIE REALIZACJE</div>
+          <div className="portfolio__item-wrap">
+            {/* {images.map((photo) => ( */}
+            <div style={getStyle(photo)} className="portfolio__item"></div>
+            <div style={getStyle(photo)} className="portfolio__item"></div>
+            <div style={getStyle(photo)} className="portfolio__item"></div>
+            <div style={getStyle(photo)} className="portfolio__item"></div>
 
-          <div style={getStyle(photo)} className="portfolio__item"></div>
-          <div style={getStyle(photo)} className="portfolio__item"></div>
-          <div style={getStyle(photo)} className="portfolio__item"></div>
-          <div style={getStyle(photo)} className="portfolio__item"></div>
+            <div style={getStyle(photo)} className="portfolio__item"></div>
+            <div style={getStyle(photo)} className="portfolio__item"></div>
+            <div style={getStyle(photo)} className="portfolio__item"></div>
+            <div style={getStyle(photo)} className="portfolio__item"></div>
 
-          {/* ))} */}
+            {/* ))} */}
+          </div>
         </div>
       </div>
-    </div>
+      {/* <Lightbox
+        images={[
+          {
+            src: photo,
+            title: "image title",
+            description: "image description",
+          },
+        ]}
+        renderImageFunc={(idx, image, toggleLightbox, width, height) => {
+          return (
+            <img
+              key={idx}
+              src={image.src}
+              className="img-circle"
+              style={{ width: width, height: height }}
+              onClick={toggleLightbox.bind(null, idx)}
+            />
+          );
+        }}
+      /> */}
+    </>
   );
 };
 
