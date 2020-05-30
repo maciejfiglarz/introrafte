d31d6a2058513e0619823b840439ff6c01e2269e
Maciej Figlarz <maciejfiglarz333@gmail.com>
2020-05-30 19:15:08 +0200

    build

---
+++ build/precache-manifest.c5786577ae2f7fafa7172fbf950fe09d.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3db706a15814118dc91383d5b95b689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d559211b22d559602fa",
+    "url": "/static/css/2.b0d70484.chunk.css"
+  },
+  {
+    "revision": "48f40c1de91be78953bb",
+    "url": "/static/css/main.aa3fbfd2.chunk.css"
+  },
+  {
+    "revision": "2d559211b22d559602fa",
+    "url": "/static/js/2.9a6dca10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9a6dca10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f40c1de91be78953bb",
+    "url": "/static/js/main.4147e4f4.chunk.js"
+  },
+  {
+    "revision": "95bd041ce2f71b167ff0",
+    "url": "/static/js/runtime-main.cd31f6ae.js"
+  },
+  {
+    "revision": "5441b68552ff636c709feef9aeb587fe",
+    "url": "/static/media/1.5441b685.png"
+  },
+  {
+    "revision": "cb36b4daf236e61b2b91340c761df631",
+    "url": "/static/media/19366.cb36b4da.jpg"
+  },
+  {
+    "revision": "594921d645b3a9513eca6fec87309bba",
+    "url": "/static/media/2.594921d6.png"
+  },
+  {
+    "revision": "37dcbd35dfa3bbd8b4f69bb8ac5e55f0",
+    "url": "/static/media/3.37dcbd35.png"
+  },
+  {
+    "revision": "9b057f3997b4beea88a0a7368501f4da",
+    "url": "/static/media/3.9b057f39.png"
+  },
+  {
+    "revision": "c2d4333dfaf9876d3f9d8754903f835d",
+    "url": "/static/media/4.c2d4333d.png"
+  },
+  {
+    "revision": "bb2b792980096406c3679f33a2f444ea",
+    "url": "/static/media/5.bb2b7929.png"
+  },
+  {
+    "revision": "a58fc5cebc188437b740b3ae980ab8f7",
+    "url": "/static/media/6.a58fc5ce.png"
+  },
+  {
+    "revision": "ae3ed0615bb7bda192bb908d25b01274",
+    "url": "/static/media/6.ae3ed061.png"
+  },
+  {
+    "revision": "c8bec98fdc605aaeb0b63f73ffd540b6",
+    "url": "/static/media/7.c8bec98f.png"
+  },
+  {
+    "revision": "5bd409f87210ed740c876e6f709ee851",
+    "url": "/static/media/8.5bd409f8.png"
+  },
+  {
+    "revision": "c5baa9ab9e36371ca05074d7f5cb7874",
+    "url": "/static/media/about.c5baa9ab.svg"
+  },
+  {
+    "revision": "537ed1920fa9ba55a2bc81f051198fb4",
+    "url": "/static/media/bg.537ed192.svg"
+  },
+  {
+    "revision": "d38cd7bc055322dbadea0329cf82cd7c",
+    "url": "/static/media/bg1.d38cd7bc.png"
+  },
+  {
+    "revision": "7638dc7b1c53851c27d35c28b7c1d721",
+    "url": "/static/media/bg2.7638dc7b.png"
+  },
+  {
+    "revision": "450c849c31fd2543704329e3716ff5c4",
+    "url": "/static/media/contact.450c849c.svg"
+  },
+  {
+    "revision": "30a7238e418ca1a3dbd28ff42e41c298",
+    "url": "/static/media/footer_background.30a7238e.jpg"
+  },
+  {
+    "revision": "e46d04e493911c851ba54cf5ad2e3f5a",
+    "url": "/static/media/goldenmed.e46d04e4.png"
+  },
+  {
+    "revision": "7281b42f874b7ecb8fbecc1b6d7c488c",
+    "url": "/static/media/logo.7281b42f.png"
+  },
+  {
+    "revision": "f3d18f7b3c58452c5c5312d240861e4b",
+    "url": "/static/media/pl.f3d18f7b.svg"
+  },
+  {
+    "revision": "1a6b7671f86000cfb5dc98a086b66183",
+    "url": "/static/media/print.1a6b7671.jpg"
+  },
+  {
+    "revision": "7bcb9aa35e6336018415524c3d466fe0",
+    "url": "/static/media/print2.7bcb9aa3.jpg"
+  },
+  {
+    "revision": "88eeefda5725895956ee300bd6b2b7c6",
+    "url": "/static/media/print3.88eeefda.jpg"
+  },
+  {
+    "revision": "4be9313439f8476616d9d5d8226acc90",
+    "url": "/static/media/print4.4be93134.jpg"
+  },
+  {
+    "revision": "59d7cded54d968a6f7f6ef1f9ad83719",
+    "url": "/static/media/sofia.59d7cded.jpg"
+  },
+  {
+    "revision": "b243001292a24a8827b854ac6dc050b6",
+    "url": "/static/media/warsaw.b2430012.jpg"
+  }
+]);
